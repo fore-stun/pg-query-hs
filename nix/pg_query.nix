c1f03cd88fff2cac6ecf3174d904824927825073
David Baynard <git@baynard.dev>
2024-08-09 21:03:56 +0100

    Patch libpq_query on linux too

--- nix/pg_query.nix
+++ nix/pg_query.nix
@@ -1,6 +1,7 @@
 { lib
 , haskell
 , haskellPackages
+, hostPlatform
 , libpg_query
 }:
 let
@@ -30,20 +31,27 @@ let
       inherit (haskell.lib) compose;
 
       overrider = old: {
-        preBuild = old.preBuild or "" + ''
+        preBuild = old.preBuild or "" + (if hostPlatform.isLinux then ''
+          ln -sv "${libpg_query}/lib/libpg_query.so" "libpg_query.so.${pg_suffix}"
+        '' else ''
           ln -sv "${libpg_query}/lib/libpg_query.dylib" "libpg_query.${pg_suffix}.dylib"
-        '';
-        postFixup = old.postFixup or "" + ''
+        '');
+        postFixup = old.postFixup or "" + (if hostPlatform.isLinux then ''
+          patchelf \
+            --replace-needed "libpg_query.so.${pg_suffix}" "${libpg_query}/lib/libpg_query.so" \
+            "$out/bin/example"
+        '' else ''
           /usr/bin/install_name_tool -change "libpg_query.${pg_suffix}.dylib" \
             "${libpg_query}/lib/libpg_query.dylib" "$out/bin/example"
 
           /usr/bin/codesign --force -s - "$out/bin/example"
-        '';
+        '');
       };
 
     in
     lib.flip lib.pipe [
       (compose.disableCabalFlag "default_paths")
+      (compose.appendConfigureFlag "--extra-lib-dirs=.")
       (drv: drv.overrideAttrs overrider)
     ]
       (haskellPackages.callCabal2nix "pg-query" src { pg_query = libpg_query; });
